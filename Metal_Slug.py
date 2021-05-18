6116eefe7ee90af1a9a7aebfff07ab51a684d76f
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2021-05-18 14:34:21 -0300

    Estruturação do Jogo

---
+++ Metal_Slug.py
@@ -0,0 +1,21 @@
+# ===== Inicialização =====
+#Import
+import pygame as pg
+#Init
+pg.init()
+#Tela principal
+WIDTH = 304
+HEIGHT =224
+window = pg.display.set_mode ((WIDTH,HEIGHT))
+pg.display.set_caption("Metal Slug Remake")
+
+
+#estrutura de dados
+# ===== Game Loop =====
+while game:
+    #Trata Eventos
+    #Saidas
+    #Update
+
+#===== Finalização =====
+pg.quit()

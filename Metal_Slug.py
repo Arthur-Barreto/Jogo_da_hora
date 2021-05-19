310f1749d82e5b8b19dae8654274d3eb5e40328b
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2021-05-19 17:37:29 -0300

    Nenhuma mudança

--- Metal_Slug.py
+++ Metal_Slug.py
@@ -3,6 +3,7 @@
 import pygame as pg
 import random
 import time
+from Tela_inicial import start_screen
 
 #Init
 pg.init()
@@ -18,7 +19,7 @@ assets={} #Dicionario Assets Jogo
 startsc_anim=[] #Lista Animações tela inicial
 for i in range (2):
     #Arquivos para animação - 0 a 1
-    nome_arquivo = "Jogo_da_hora/Tela Inicial/TIS{}.png".format(i)
+    nome_arquivo = "Tela Inicial/TIS{}.png".format(i)
     img = pg.image.load(nome_arquivo).convert()
     img = pg.transform.scale (img,(0,0))
     startsc_anim.append(img)
@@ -27,8 +28,7 @@ assets["startsc_anim"]=startsc_anim
 #-----Estrutura de dados
 game = True
 start_screen = True
-#Classes
-class StartScreen(pg.sprite.Sprite):
+
     
 # == Start Screen ==
 while start_screen:

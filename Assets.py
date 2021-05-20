9d8917f2fb4e312b2abb9fa423d3b54e793e1a11
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2021-05-20 14:09:57 -0300

    PF_Anim (Andar para Frente)

--- Assets.py
+++ Assets.py
@@ -2,6 +2,10 @@ import pygame as pg
 import os
 Start_screen = "startsc_anim"
 
+#Variaveis
+HEIGHT_P= 32
+WIDTH_P= 32
+
 def load_assets():
     #Definindo Assets
     assets={}
@@ -11,7 +15,13 @@ def load_assets():
     start_sc.append(pg.image.load("Tela Inicial/TIS1.png").convert())
     assets["startsc_anim"] = start_sc
     #Animação Jogador - Andando Frente
-    
+    PF_Anim = []
+    for e in range(0,23):
+        nome_arquiva = "Jogador/PlayerWalking/{}.png".format[e]
+        img = pg.image.load(nome_arquiva).convert()
+        img = pg.transform.scale(img,(HEIGHT_P,WIDTH_P))
+        PF_Anim.append(img)
+    assets["player"] = PF_Anim
     #Animação Jogador - Andando trás
     #Animação Jogador - Pulando
     print (assets["startsc_anim"])

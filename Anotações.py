d99dac520369ca5d22b91f576824aa563c20b571
LuanCintra <luancintra1234@outlook.com>
2021-05-21 23:51:50 -0300

    Pasta inimigos>Animação Tiro Direita e Esquerda

---
+++ Anotações.py
@@ -0,0 +1,11 @@
+#Inimigos:
+
+#---TEMOS----
+# Animação Atirando Esquerda e Direita
+
+#---FALTANDO----
+#Animação Morrendo Esquerda e Direita
+#Animação Correndo Esquerda e Direita
+
+
+#Ultima modify = Inimigos >> Soldado inimigo >> Atirando Direita // Atirando Esquerda
